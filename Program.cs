071ae708440e2f7bd443e27a204fb50198c932bd
FishThePumpkin <39113773+FishThePumpkin@users.noreply.github.com>
2018-05-09 16:01:19 +1000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,80 @@
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Gusion_Bot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public async Task RunBotAsync()
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string botToken = "NDQzNTg4ODE0NDczMDAzMDA4.DdQZOA.ndWOJBnPU3UTfvmq_abgYDLUvg8";
+
+
+            //event subscriptions
+
+            _client.Log += Log;
+
+            await RegisterCommandAsync();
+
+            await _client.LoginAsync(Discord.TokenType.Bot, botToken);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task RegisterCommandAsync()
+        {
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message is null || message.Author.IsBot) return;
+
+
+            int argPos = 0;
+
+            if (message.HasStringPrefix("gusion", ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))
+            {
+                var context = new SocketCommandContext(_client, message);
+
+                var result = await _commands.ExecuteAsync(context, argPos, _services);
+
+                if (!result.IsSuccess)
+                    Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
+client.login(process.env.BOT_TOKEN);
